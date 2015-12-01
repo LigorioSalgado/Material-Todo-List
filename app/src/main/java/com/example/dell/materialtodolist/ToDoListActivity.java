4df24c7fddc4478b9085b68af949e51ababae0c7
LigorioSalgado <salgadolg5@hotmail.com>
2015-11-30 19:48:36 -0600

    cambios en app, sin finalizar

--- app/src/main/java/com/example/dell/materialtodolist/ToDoListActivity.java
+++ app/src/main/java/com/example/dell/materialtodolist/ToDoListActivity.java
@@ -8,8 +8,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ToDoListActivity extends AppCompatActivity {
+    private EditText item;
+    private ImageButton add;
+    private ListView dynamicListView;
+    private ArrayList<String> list;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +30,17 @@ public class ToDoListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        item = (EditText) findViewById(R.id.itemEditText);
+        add = (ImageButton) findViewById(R.id.add_item_button);
+        dynamicListView = (ListView) findViewById(R.id.itemsListView);
+        //Initializate the list and add item
+        list = new ArrayList<String>();
+        list.add("Android ATC");
+
+        //initializate the arrayAdapter
+        adapter = new ArrayAdapter<String>(ToDoListActivity.this,android.R.layout.simple_list_item_1,list);
+        //Setting the adapter to the list view
+        dynamicListView.setAdapter(adapter);
 
     }
 
